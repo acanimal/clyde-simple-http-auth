631ec4f8b19b0fd356a94df131d2eb5f361406fe
acanimal <asantiago@acuriousanimal.com>
2015-07-09 22:33:28 +0200

    Filter implementation and tests

---
+++ README.md
@@ -0,0 +1,110 @@
+# Simple HTTP Authentication Filter
+
+HTTP authentication filter for [Clyde](https://github.com/acanimal/clyde) API gateway, which allows to authenticate users both using basic and digest strategies.
+
+> Implementation is based on on [passport-http](https://github.com/jaredhanson/passport-http) module.
+
+<!-- MarkdownTOC -->
+
+- [Configuration](#configuration)
+  - [Examples](#examples)
+    - [Configured as global prefilter](#configured-as-global-prefilter)
+    - [Configured as provider prefilter](#configured-as-provider-prefilter)
+  - [Notes](#notes)
+- [License](#license)
+
+<!-- /MarkdownTOC -->
+
+## Configuration
+
+Filter accepts the configuration properties:
+
+* `realm`: A string that identifies the authentication realm.
+* `method`: Indicates the authenticated method to be used. It is a required property. Allowed values are `basic` and `digest`.
+* `consumers`: An object with the list of `user` properties and `password` values.
+
+
+## Examples
+
+### Configured as global prefilter
+
+All requests are authenticated using basic auth:
+
+```javascript
+{
+  "prefilters" : [
+    {
+      "id" : "basic-auth",
+      "path" : "simple-http-auth",
+      "config" : {
+        "realm" : "clyde",
+        "method" : "basic",
+        "consumers" : {
+          "userA" : "passwordA",
+          ...
+        }
+      }
+    }
+    ...
+  ]
+}
+```
+
+### Configured as provider prefilter
+
+Only the requests addresses to the provider are authenticated with digest method:
+
+```javascript
+{
+  "providers" : [
+    {
+      "id" : "some_provider",
+      "context" : "/some_provider",
+      "target" : "http://some_server",
+      "prefilters" : [
+        {
+          "id" : "digest-auth",
+          "path" : "simple-http-auth",
+          "config" : {
+            "realm" : "clyde",
+            "method" : "digest",
+            "consumers" : {
+              "userA" : "passwordA",
+              ...
+            }
+          }
+        }
+    },
+    ...
+  ]
+}
+```
+
+## Notes
+
+* It has no sense configure an authentication filter as a global or provider's postfilter.
+
+
+# License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Antonio Santiago (@acanimal)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ test/simple-http-auth.test.js
@@ -0,0 +1,265 @@
+"use strict";
+
+var path = require("path"),
+    supertest = require("supertest"),
+    request = require("request"),
+    expect = require("chai").expect,
+    http = require("http"),
+    clyde = require("clyde");
+
+
+describe("simple-http-auth", function() {
+
+  var server;
+
+  afterEach(function() {
+    if (server) {
+      server.close();
+    }
+  });
+
+
+  it("should fail due invalid authentication method", function(done) {
+    var logDirectory = path.join(__dirname, "../tmp");
+
+    var options = {
+      port: 8888,
+      logfile: path.join(logDirectory, "clyde.log"),
+      loglevel: "info",
+
+      prefilters: [
+        {
+          id: "simple-http-auth",
+          path: path.join(__dirname, "../lib/index.js"),
+          config: {
+            realm: "test",
+            method: "invalid-method",
+            consumers: {
+              "userA": "passwordA"
+            }
+          }
+        }
+      ],
+
+      providers: [
+        {
+          id: "id",
+          context: "/provider",
+          target: "http://server"
+        }
+      ]
+    };
+
+    // Create server with clyde's middleware options
+    try {
+      clyde.createMiddleware(options);
+      throw new Error("Should not arrive here");
+    } catch (err) {
+      expect(err.message).to.contains("simple-http-auth");
+      done();
+    }
+  });
+
+
+  it("should fail due invalid basic authentication", function(done) {
+    var logDirectory = path.join(__dirname, "../tmp");
+
+    var options = {
+      port: 8888,
+      logfile: path.join(logDirectory, "clyde.log"),
+      loglevel: "info",
+
+      prefilters: [
+        {
+          id: "simple-http-auth",
+          path: path.join(__dirname, "../lib/index.js"),
+          config: {
+            realm: "test",
+            method: "basic",
+            consumers: {
+              "userA": "passwordA"
+            }
+          }
+        }
+      ],
+
+      providers: [
+        {
+          id: "id",
+          context: "/provider",
+          target: "http://server"
+        }
+      ]
+    };
+
+    // Create server with clyde's middleware options
+    var middleware = clyde.createMiddleware(options);
+    server = http.createServer(middleware);
+    server.listen(options.port);
+
+    // Make request which expects a not authenticated error
+    supertest("http://localhost:8888")
+      .get("/foo")
+      .auth("bad-user", "bad-password")
+      .expect(401)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        expect(res.headers["www-authenticate"]).to.contains("Basic realm=\"test\"");
+        done();
+      });
+  });
+
+
+  it("should success request with basic authentication", function(done) {
+    var logDirectory = path.join(__dirname, "../tmp");
+
+    var options = {
+      port: 8888,
+      logfile: path.join(logDirectory, "clyde.log"),
+      loglevel: "info",
+
+      prefilters: [
+        {
+          id: "simple-http-auth",
+          path: path.join(__dirname, "../lib/index.js"),
+          config: {
+            realm: "test",
+            method: "basic",
+            consumers: {
+              "userA": "passwordA"
+            }
+          }
+        }
+      ],
+
+      providers: [
+        {
+          id: "id",
+          context: "/provider",
+          target: "http://server"
+        }
+      ]
+    };
+
+    // Create server with clyde's middleware options
+    var middleware = clyde.createMiddleware(options);
+    server = http.createServer(middleware);
+    server.listen(options.port);
+
+    // Make request which expects a not found error
+    supertest("http://localhost:8888")
+      .get("/foo")
+      .auth("userA", "passwordA")
+      .expect(404, done);
+  });
+
+
+  it("should fail due invalid digest authentication", function(done) {
+    var logDirectory = path.join(__dirname, "../tmp");
+
+    var options = {
+      port: 8888,
+      logfile: path.join(logDirectory, "clyde.log"),
+      loglevel: "info",
+
+      prefilters: [
+        {
+          id: "simple-http-auth",
+          path: path.join(__dirname, "../lib/index.js"),
+          config: {
+            realm: "test",
+            method: "digest",
+            consumers: {
+              "userA": "passwordA"
+            }
+          }
+        }
+      ],
+
+      providers: [
+        {
+          id: "id",
+          context: "/provider",
+          target: "http://server"
+        }
+      ]
+    };
+
+    // Create server with clyde's middleware options
+    var middleware = clyde.createMiddleware(options);
+    server = http.createServer(middleware);
+    server.listen(options.port);
+
+    // Make request which expects a not authenticated error
+    request.get("http://localhost:8888/foo", {
+      "auth": {
+        "user": "bad-username",
+        "pass": "bad-password",
+        "sendImmediately": false
+      }
+    }, function (err, res) {
+      if (err) {
+        throw err;
+      }
+      expect(res.statusCode).to.be.equal(401);
+      expect(res.headers["www-authenticate"]).to.contains("Digest realm=\"test\"");
+      done();
+    });
+  });
+
+
+  it("should success request with digest authentication", function(done) {
+    var logDirectory = path.join(__dirname, "../tmp");
+
+    var options = {
+      port: 8888,
+      logfile: path.join(logDirectory, "clyde.log"),
+      loglevel: "info",
+
+      prefilters: [
+        {
+          id: "simple-http-auth",
+          path: path.join(__dirname, "../lib/index.js"),
+          config: {
+            realm: "test",
+            method: "digest",
+            consumers: {
+              "userA": "passwordA"
+            }
+          }
+        }
+      ],
+
+      providers: [
+        {
+          id: "id",
+          context: "/provider",
+          target: "http://server"
+        }
+      ]
+    };
+
+    // Create server with clyde's middleware options
+    var middleware = clyde.createMiddleware(options);
+    server = http.createServer(middleware);
+    server.listen(options.port);
+
+    // Make request which expects a not found error
+    request.get("http://localhost:8888/foo", {
+      "auth": {
+        "user": "userA",
+        "pass": "passwordA",
+        "sendImmediately": false
+      }
+    }, function (err, res) {
+      if (err) {
+        throw err;
+      }
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+});
